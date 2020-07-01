b7cf7e24267900edf9d1e1683fa98aba64049eac
ArthurDentt <ajconne2@ncsu.edu>
2020-07-01 14:19:38 -0400

    New C2 analysis file for Proton that does jackknife taking source locations and positions into account for more statistical accuracy also only makes 1 graph :)

--- C2 Proton.jl
+++ C2 Proton.jl
@@ -18,11 +18,7 @@ end
 
 # Defining a function to go from scientific notation to floats
 function value(str)
-    parts = split(str,"e")
-    digits = parse(Float64,parts[1])
-    power = parse(Int,parts[2])
-    number=digits*(10.0^(power))
-    return(number)
+    parse(Float64,str)
 end
 
 # Defining a function to find the SE of a vector of Jackknife Expectation values
@@ -60,7 +56,7 @@ end
 function reshapevector(vector, i)
     newvector = zeros(length(vector))
     for j in range(1,length(vector),step=1)
-        if (i+j <= length(vector))
+        if ((i+j) <= length(vector))
             newvector[j] = vector[i+j]
         else
             newvector[j] = vector[(i+j)%length(vector)]
@@ -84,9 +80,15 @@ function Emass(C2vector)
     return(Emassvector)
 end
 
-slices = [27:40,27:40,27:40,23:40,25:38,25:35,24:42]
+################################################################################
+########################### END OF FUNCTIONS ###################################
+################################################################################
 
 # For loop Iterating over the "TX" files indicating source time
+numfiles=7*16*39
+tempdata = []
+alldata = zeros(ComplexF64,(numfiles,64))
+global C2 = []
 @progress (name="Plotting...") for i in range(0,48,step=8)
     Tindex=i
     # I'm going to leave my directory path here so you can see how this works
@@ -99,12 +101,7 @@ slices = [27:40,27:40,27:40,23:40,25:38,25:35,24:42]
         dir = string(dir, "\\", rdir)
         cd(dir)
 
-        Operatormatrix = [] # It's a matrix full of operator values
-        C2 = [] # Defining C₂(t,μₐ) -> μₐ is Gauge Config α
-        realEC2=[] # Real part of expectation value of C₂(t)
-        jackreplicates = [] # matrix of jackknife replicates
-        stderror = [] # error vector sqrt(variance(jackknife))
-        global jackestimates = []
+        global filevector = []
 
         # Starting the main loop running over each text file ending in .dat.###
         for k in range(0,42,step=1)
@@ -126,71 +123,42 @@ slices = [27:40,27:40,27:40,23:40,25:38,25:35,24:42]
             times=[]; # times
             linematrices=[] # matrix full of pieces of each line in the relevant data file
 
+
             # pushing split lines into a matrix
             for l in range(2,length(lines)-1,step=1)
                 push!(linematrices,split(lines[l]))
                 push!(times,parse(Int,linematrices[l-1][1]))
             end
 
-            # Define operator arrays
             Op1 = zeros(ComplexF64,(length(times)))
-
-            # For now I am not using Operator 2 (it is the negative parity C₂(t))
-            # Un-comment all similar lines to use it
-            #Op2 = zeros(ComplexF64,(length(times)))
 
             # Fill operator arrays
             for l in range(1,length(linematrices),step=1)
                 Op1[l]=value(linematrices[l][2])+(value(linematrices[l][3]))im
 
-                # For now I am not using Operator 2
-                #Op2[i]=value(linematrices[i][4])+(value(linematrices[i][5]))im
             end
 
-            push!(Operatormatrix,Op1[:])
-            #println("Pushed Op1 to Operatormatrix, Filename: \"nuc3pt.dat.$findex\"")
+            push!(filevector,Op1[:])
 
         end
 
-        # Rearrange Operatormatrix into C₂(t,μₐ)
-        for k in range(1,length(Operatormatrix[1]),step=1)
-            timeslice=[] # Slice t=l across all μₐ
-            for l in range(1,length(Operatormatrix),step=1)
-                push!(timeslice,Operatormatrix[l][k])
+        # Rearrange Operatormatrix into C₂(μₐ,t)
+        for k in range(1,length(filevector),step=1)
+            gaugeconfig=[] # Slice t=l across all μₐ
+            for l in range(1,length(filevector[1]),step=1)
+                push!(gaugeconfig,filevector[k][l])
             end
-            push!(C2,timeslice)
+            push!(C2,gaugeconfig)
         end
 
-        # Filling jackreplicates matrix
-        for k in range(1,length(C2),step=1)
-            jackknifevector=Jackrep(C2[k])
-            push!(jackreplicates,jackknifevector)
+        for i in range(1,length(C2),step=1)
+            push!(tempdata,C2[i])
         end
 
-        # Filling Standard error matrix
-        for k in range(1,length(jackreplicates),step=1)
-            SE = JackSE(jackreplicates[k])
-            push!(stderror,SE)
-        end
-
-        # Filling jackknife estimate vector
-        for k in range(1,length(jackreplicates),step=1)
-            push!(jackestimates,real(mean(jackreplicates[k])))
-        end
-
-        if (j==1)
-
-            slice = 19:length(jackestimates)-19
-            resjack = reshapevector(jackestimates,i)
-            reserror = reshapevector(stderror,i)
-
-        end
-
-
-        cd("C:\\Users\\Drew\\github\\SULI-LQCD")
-        global dataoutfile = open("C2ProtonData.txt","a")
-        write(dataoutfile,string(jackestimates,"\n", stderror, "\n"))   # Not Used for now #
-        close(dataoutfile)
+        #cd("C:\\Users\\Drew\\github\\SULI-LQCD")
+        #global dataoutfile = open("C2ProtonData TESTING.txt","a")
+        #write(dataoutfile,string(jackestimates,"\n", stderror, "\n"))   # Not Used for now #
+        #close(dataoutfile)
 
         # This is where finding C₂(t) ends #
 
@@ -203,6 +171,39 @@ slices = [27:40,27:40,27:40,23:40,25:38,25:35,24:42]
         cd(dir)
     end
 end
+close(test_file)
+for i in range(1,length(C2),step=1)
+    alldata[i,:]=C2[i]
+end
+alldata=real.(alldata)
+for i in range(1,length(alldata[:,1]),step=1)
+    Tindex = (Int(floor((i-1)/624)*8))
+    alldata[i,:] = reshapevector(alldata[i,:],Tindex)
+end
 
+for i in range(1,length(alldata[1,:]),step=1)
+    alldata[:,i] = Jackrep(alldata[:,i])
+end
+
+stderrors = zeros(length(alldata[1,:]))
+
+for i in range(1,length(alldata[1,:]),step=1)
+    stderrors[i] = JackSE(alldata[:,i])
+end
+
+finalvals = zeros(length(alldata[1,:]))
+for i in range(1,length(alldata[1,:]),step=1)
+    finalvals[i]=mean(alldata[:,i])
+end
+
+cd("C:\\Users\\Drew\\github\\SULI-LQCD")
+global dataoutfile = open("C2ProtonData.txt","a")
+write(dataoutfile,string(finalvals,"\n", stderrors, "\n"))
+close(dataoutfile)
+
+cd("C:\\Users\\Drew\\github\\SULI-LQCD\\FinalPlots")
+plot(1:length(finalvals),finalvals,markerstrokecolor=(:black),marker=(:circle),yerror=stderrors,legend=false,dpi=300)
+xlabel!("t");ylabel!("Re(<C₂>)");title!("Proton Re(<C₂>)(t)")
+savefig("Proton C2 Plot.png")
 # Note: because this is all in a big loop, you won't see anything in the workspace
 # variable explorer :(
