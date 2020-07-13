60f74ad5e65321586966aacbe533f12aac99d3ee
ArthurDentt <ajconne2@ncsu.edu>
2020-07-13 13:30:36 -0400

    Cleaning Axial Charge, Commenting, Condensing for-loops

--- C3 Axial Charge.jl
+++ C3 Axial Charge.jl
@@ -88,25 +88,6 @@ function Emass(C2vector)
     return(Emassvector)
 end
 
-function EmassSE(C2vector,C2SEvector)
-    EmassSEvector = []
-    EmassSE = 0
-    for i in range(1,length(C2vector),step=1)
-        if (i==length(C2vector))
-            a = (C2SEvector[i]/(C2vector[1]))
-            b = (C2vector[i]*C2SEvector[1]/(C2vector[1]^2))
-            EmassSE = sqrt(a^2 + b^2)*C2vector[1]/C2vector[i]
-        else
-            a = (C2SEvector[i]/(C2vector[i+1]))
-            b = (C2vector[i]*C2SEvector[i+1]/(C2vector[i+1]^2))
-            EmassSE = sqrt(a^2 + b^2)*C2vector[i+1]/C2vector[i]
-        end
-        push!(EmassSEvector,EmassSE)
-    end
-    EmassSEvector=real(log.(Complex.(EmassSEvector)))
-    return(EmassSEvector)
-end
-
 ################################################################################
 ########################### END OF FUNCTIONS ###################################
 ################################################################################
@@ -216,9 +197,14 @@ for i in range(1,39,step=1) # Iterate over gauge configurations
     binnedmeans[i,:] = meanvector
 end
 
-# Turn binnedmeans into binned Jack replicates
+# Turn binnedmeans into binned Jack replicates, renormalizing by sourcedata
+# Then populating Jack estimators and standard errors
+stderrors = zeros(length(binnedmeans[1,:]))
+finalvals = zeros(length(binnedmeans[1,:]))
 for i in range(1,length(binnedmeans[1,:]),step=1)
-    binnedmeans[:,i] = Jackrep(binnedmeans[:,i])
+    binnedmeans[:,i] = Jackrep(binnedmeans[:,i])/(abs(sourcedata[i])*3.2)
+    finalvals[i]=mean(binnedmeans[:,i])
+    stderrors[i] = JackSE(binnedmeans[:,i])
 end
 
 # saving gA replicates from plateau to file for gA/gV plot
@@ -232,60 +218,7 @@ end
 write(dataoutfile,gAratiostring)
 close(dataoutfile)
 
-# This renormalizes by sourcedata
-for i in range(1,length(binnedmeans[:,1]),step=1)
-    binnedmeans[i,:] = binnedmeans[i,:]/(abs(sourcedata[i])*3.2)
-end
-
-fitmassreps = zeros((2,length(binnedmeans[:,1])))
-plateau = 7:11
-model(t,p) = p[1]*exp.(-p[2]*t)
-for i in range(1,length(binnedmeans[:,1]),step=1)
-    fit = curve_fit(model,plateau,binnedmeans[i,plateau],[-1000000,.71])
-    fitmassreps[:,i] = fit.param
-end
-
-EffectiveMass = mean(fitmassreps[2,:])
-Amplitude = mean(fitmassreps[1,:])
-EffectiveMassSE = JackSE(fitmassreps[2,:])
-
-Fitfunction(t) = Amplitude*ℯ^(-EffectiveMass*t)
-
-stderrors = zeros(length(binnedmeans[1,:]))
-
-for i in range(1,length(binnedmeans[1,:]),step=1)
-    stderrors[i] = JackSE(binnedmeans[:,i])
-end
-
-finalvals = zeros(length(binnedmeans[1,:]))
-for i in range(1,length(binnedmeans[1,:]),step=1)
-    finalvals[i]=mean(binnedmeans[:,i])
-end
-
-Effmassrep=zeros((39,length(alldata[1,:])))
-
-for i in range(1,length(binnedmeans[:,1]),step=1)
-    Effmassrep[i,:]=Emass(binnedmeans[i,:])
-end
-
-Effmass = zeros(length(Effmassrep[1,:]))
-EffmassSE = zeros(length(Effmassrep[1,:]))
-
-for i in range(1,length(Effmassrep[1,:]),step=1)
-    Effmass[i]=mean(Effmassrep[:,i])
-    EffmassSE[i] = JackSE(Effmassrep[:,i])
-end
-
-# Finding χ² of our fit
-chisq = 0
-for i in plateau
-    global chisq += ((finalvals[i] - Fitfunction(i))^2)/(stderrors[i]^2)
-end
-chisq = chisq / 2
-println("χ²/dof = $chisq")
-
-
 cd("C:\\Users\\Drew\\github\\SULI-LQCD")
 global dataoutfile = open("C3AxialChargeData.txt","a") #saving data to file -> C2, C2 error, m*, m* error
-write(dataoutfile,string(finalvals,"\n", stderrors, "\n", Effmass, "\n", EffmassSE, "\n", EffectiveMass, "\n", EffectiveMassSE, "\n", "χ²=$chisq", "\n"))
+write(dataoutfile,string(finalvals,"\n", stderrors, "\n", Effmass, "\n", EffmassSE, "\n"))
 close(dataoutfile)
