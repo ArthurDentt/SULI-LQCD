7e656ac603eb39dfbc4a26635cb3e9062a5dc93f
ArthurDentt <ajconne2@ncsu.edu>
2020-07-13 14:34:43 -0400

    cleaned protongg breaking long lines

--- C2 ProtonGG.jl
+++ C2 ProtonGG.jl
@@ -255,5 +255,6 @@ println("χ²/dof = $chisq")
 cd("C:\\Users\\Drew\\github\\SULI-LQCD")
 global dataoutfile = open("C2ProtonGGData.txt","a")
 #saving data to file -> C2, C2 error, m*, m* error,M*E , M*SE E, C2(T) replicates, χ²
-write(dataoutfile,string(finalvals,"\n", stderrors, "\n", Effmass, "\n", EffmassSE, "\n", EffectiveMass, "\n", EffectiveMassSE, "\n", sourcereps, "\n", "χ²=$chisq", "\n"))
+write(dataoutfile,string(finalvals,"\n", stderrors, "\n", Effmass, "\n", EffmassSE,
+    "\n", EffectiveMass, "\n", EffectiveMassSE, "\n", sourcereps, "\n", "χ²=$chisq", "\n"))
 close(dataoutfile)
