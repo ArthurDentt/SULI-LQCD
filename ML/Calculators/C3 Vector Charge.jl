a197e0e93413a3e873b42701518dddf137425093
ArthurDentt <ajconne2@ncsu.edu>
2020-07-28 14:08:31 -0400

    upadted plots and plotters to cmpare real and fake data coupling plots

--- ML/Calculators/C3 Vector Charge.jl
+++ ML/Calculators/C3 Vector Charge.jl
@@ -4,15 +4,14 @@ using LsqFit
 include("C:\\Users\\Drew\\github\\SULI-LQCD\\myfunctions.jl")
 
 function MLVector(plotrange)
-    dir0="C:\\Users\\Drew\\Desktop\\FakeData"
-    cd(dir0)
+    dirF="C:\\Users\\Drew\\Desktop\\FakeData"
+    dirR="C:\\Users\\Drew\\Desktop\\RealData"
     plotdir="C:\\Users\\Drew\\github\\SULI-LQCD\\ML\\FinalPlots"
 
+    # FAKE DATA PROCESS
+    cd(dirF)
     filelist = readdir()
-
-    datamatrix = zeros((length(filelist),64))
-
-    cd(dir0)
+    global datamatrixF = zeros((length(filelist),64))
     fileindex = 0
     @progress (name = "$fileindex / $(length(filelist))") for i in filelist
         fileindex += 1
@@ -29,28 +28,65 @@ function MLVector(plotrange)
         lines=split(reldata,"\n")
 
         global linematrices=[] # matrix full of pieces of each line in the relevant data file
-        global C2 = []
+        global C2 = [] # one function C2(t) at gauge config μₐ
 
         # pushing split lines into a matrix
         for i in range(6,length(lines)-1,step=1)
             push!(linematrices,split(lines[i]))
             push!(C2, (value(linematrices[i-5][2])-value(linematrices[i-5][4])) ) # U-D contribution
         end
-
-        datamatrix[fileindex,:] = C2
+        datamatrixF[fileindex,:] = C2 # row in datamatrix indicates gauge config
         close(test_file)
     end
 
-    Jackreplicates = [Jackrep(datamatrix[:,i]) for i in range(1,length(datamatrix[1,:]),step=1)]
-    Jackestimates = [mean(i) for i in Jackreplicates]
-    Stderrors = [JackSE(i) for i in Jackreplicates]
+    # REAL DATA PROCESS
+    cd(dirR)
+    filelist = readdir()
+    global datamatrixR = zeros((length(filelist),64))
+    fileindex = 0
+    @progress (name = "$fileindex / $(length(filelist))") for i in filelist
+        fileindex += 1
+        try
+            global test_file=open(i,"r+");
+        catch (SystemError)
+            println("No file named \"$i\" exists.")
+            # Un-comment to see what files are missing, I don't really care though
+            continue
+        end
+
+        readuntil(test_file, "G4", keep = false)
+        reldata = readuntil(test_file, "END_NUC3PT", keep=false)
+        lines=split(reldata,"\n")
+
+        global linematrices=[] # matrix full of pieces of each line in the relevant data file
+        global C2 = [] # one function C2(t) at gauge config μₐ
+
+        # pushing split lines into a matrix
+        for i in range(6,length(lines)-1,step=1)
+            push!(linematrices,split(lines[i]))
+            push!(C2, (value(linematrices[i-5][2])-value(linematrices[i-5][4])) ) # U-D contribution
+        end
+        datamatrixR[fileindex,:] = C2 # row in datamatrix indicates gauge config
+        close(test_file)
+    end
+
+    JackreplicatesF = [Jackrep(datamatrixF[:,i]) for i in range(1,length(datamatrixF[1,:]),step=1)]
+    JackestimatesF = [mean(i) for i in JackreplicatesF]
+    StderrorsF = [JackSE(i) for i in JackreplicatesF]
+
+    JackreplicatesR = [Jackrep(datamatrixR[:,i]) for i in range(1,length(datamatrixR[1,:]),step=1)]
+    JackestimatesR = [mean(i) for i in JackreplicatesR]
+    StderrorsR = [JackSE(i) for i in JackreplicatesR]
 
     cd(plotdir)
 
-    scatter(plotrange[1]-1:plotrange[end]-1,Jackestimates[plotrange],marker=(:x),markercolor=(:red),
-        linecolor=(:red),markerstrokecolor=(:red),yerror=Stderrors[plotrange],
+    scatter(plotrange[1]-1:plotrange[end]-1,JackestimatesR[plotrange],marker=(:x),markercolor=(:red),
+        linecolor=(:red),markerstrokecolor=(:red),yerror=StderrorsR[plotrange],
         dpi=600,grid=false,frame=(:box), foreground_color_legend = nothing, background_color_legend=nothing,
-        label = "170 MeV AMA", legend = ((.85,.95)), legendfontsize = 7)
+        label = "REAL (170 MeV AMA)", legend = ((.75,.95)), legendfontsize = 7)
+    scatter!(plotrange[1]-1:plotrange[end]-1,JackestimatesF[plotrange],marker=(:x),markercolor=(:green),
+        linecolor=(:green),markerstrokecolor=(:green),yerror=StderrorsF[plotrange],
+        label = "ML (170 MeV AMA)")
     xlabel!("τ");ylabel!("gᵥ");title!("Vector Charge")
     savefig("Vector Charge C3 Plot.png")
 
