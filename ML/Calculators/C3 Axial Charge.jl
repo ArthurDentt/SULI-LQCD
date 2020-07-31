2832396516cb790ecb7e96eded79b2b2f1cdc7ff
ArthurDentt <ajconne2@ncsu.edu>
2020-07-30 21:39:07 -0400

    did the same for axial as vector charge

--- ML/Calculators/C3 Axial Charge.jl
+++ ML/Calculators/C3 Axial Charge.jl
@@ -107,18 +107,67 @@ function MLAxial(plotrange)
     JackestimatesR = [mean(i) for i in JackreplicatesR]
     StderrorsR = [JackSE(i) for i in JackreplicatesR]
 
-    Ratioestimate = mean(JackestimatesR[plotrange]./JackestimatesF[plotrange])
+    # Finding fake and real Charge Jackknife replicates
+    plateaulength = 6
+    ChargerepsF = zeros((39,plateaulength))
+    ChargerepsR = zeros((39,plateaulength))
+    for i in range(3,8,step=1) # 2->7
+        ChargerepsF[:,i-2] = JackreplicatesF[i]
+        ChargerepsR[:,i-2] = JackreplicatesR[i]
+    end
+    ChargerepsF = [mean(ChargerepsF[i,:]) for i in range(1,39,step=1)]
+    ChargerepsR = [mean(ChargerepsR[i,:]) for i in range(1,39,step=1)]
+
+    # Finding fake and real Charge estimates
+    ChargeF = mean(ChargerepsF)
+    ChargeR = mean(ChargerepsR)
+
+    # Finding fake and real Charge SE
+    ChargeSEF = JackSE(ChargerepsF)
+    ChargeSER = JackSE(ChargerepsR)
+
+    # Rounding fake and real charges and SE's to 3 digits for plot viewing
+    sigChargeF = round(ChargeF,digits=3)
+    sigChargeR = round(ChargeR,digits=3)
+    sigChargeSEF = round(ChargeSEF,digits=3)
+    sigChargeSER = round(ChargeSER,digits=3)
+
+    # Calculating fake and real chisquared values
+    chisqF = 0
+    chisqR = 0
+    for i in range(1,plateaulength,step=1)
+        chisqF += ((ChargeF-JackestimatesF[i+2]))^2  / (StderrorsF[i+2]) / (plateaulength-1)
+        chisqR += ((ChargeR-JackestimatesR[i+2]))^2  / (StderrorsR[i+2]) / (plateaulength-1)
+    end
+    chisqF = round(chisqF,digits=3)
+    chisqR = round(chisqR,digits=3)
 
     cd(plotdir)
 
+    # Setting plot boundaries
+    xtickvals = [i for i in range(0,9,step=1)]
+    ytickvals = [(1.45 +.05*i) for i in range(0,10,step=1)]
+    ytick0 = ["" for i in range(1,length(ytickvals),step=1)]
+    xtick0 = ["" for i in range(1,length(xtickvals),step=1)]
+
+    # Plotting ML and Real comparison
     scatter(plotrange[1]-1:plotrange[end]-1,JackestimatesR[plotrange],marker=(:x),markercolor=(:purple),
         linecolor=(:purple),markerstrokecolor=(:purple),yerror=StderrorsR[plotrange],
-        dpi=600,grid=false,frame=(:box), foreground_color_legend = nothing, background_color_legend=nothing,
-        label = "REAL (170 MeV AMA)", legend = ((.75,.95)), legendfontsize = 7)
+        dpi=600,grid=false,xlims=(xtickvals[1],xtickvals[end]),
+        ylims=(ytickvals[1],ytickvals[end]),xticks=xtickvals,yticks=ytickvals,frame=(:box),
+        foreground_color_legend = nothing, background_color_legend=nothing, label = "REAL",
+        legend = ((.85,.95)), legendfontsize = 7)
     scatter!(plotrange[1]-1:plotrange[end]-1,JackestimatesF[plotrange],marker=(:x),markercolor=(:green),
         linecolor=(:green),markerstrokecolor=(:green),yerror=StderrorsF[plotrange],
-        label = "ML (170 MeV AMA)")
+        label = "ML")
     xlabel!("τ");ylabel!("gₐ");title!("Axial Charge")
+    plot!(twinx(), xmirror=:true,grid=:false,ylims=(ytickvals[1],ytickvals[end]),
+        xlims=(xtickvals[1],xtickvals[end]),xticks = (xtickvals,xtick0),
+        yticks=(ytickvals,ytick0))
+    annotate!(.34, 1.93, text("Vector Charge Estimates:",6, :left, :black))
+    annotate!(.17, 1.91, text("Real: $sigChargeR($sigChargeSER) | χ²ᵥ = $chisqF ",6, :left, :purple))
+    annotate!(.18, 1.89, text("ML: $sigChargeF($sigChargeSEF) | χ²ᵥ = $chisqR ",6, :left, :green))
+
     savefig("Axial Charge C3 Plot.png")
 
     return("Done with ML Axial!")
